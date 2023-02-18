f5f38371bbe2fc997b9033491f908f8cada602a2
Alex Panaev <panaev.as@gmail.com>
2023-02-18 20:51:07 +0400

    PageObject:updated testLogin(),added LoginPage

--- src/test/java/classes/LoginTest.java
+++ src/test/java/classes/LoginTest.java
@@ -1,9 +1,10 @@
 package classes;
 
-import org.assertj.core.api.Condition;
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.enabled;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,17 +12,20 @@ import static org.openqa.selenium.By.id;
 
 public class LoginTest {
 
+    @BeforeClass
+    public static void setUp(){
+        Configuration.baseUrl = "https://app.qase.io";
+    }
+
     @Test
     public void testLogin() {
         //1.open URL
-        open("https://app.qase.io/login");
+        open("/login");
         //2.Find the elements by ID. FullXpath: //input[@id='inputEmail'/'inputPassword']
         $(id("inputEmail"))
-
                 //3.shouldBe(Condition.enabled). => we can verify that elem is enabled
                 //and waiter is here too
                 .shouldBe(enabled)
-
                 //send email to the field
                 .sendKeys("mrgmgrv@gmail.com");
 
@@ -31,9 +35,9 @@ public class LoginTest {
         //'Should' is not checking! it's just for sure
         $(id("createButton")).shouldBe(visible);
 
-        //only assert is check
-        assertThat($(id("createButton")).getText()).isNotNull()
-                                                   .isEqualTo("Create new project");
+        //only assert checks
+        assertThat($(id("createButton")).getText()).isNotNull().isEqualTo("Create new project");
 
+       // final boolean test = $((WebElement) text("Test")).isDisplayed();//a[text()="Test"]
     }
 }
