c098bca50e404e07115b9a33d68a81dd72a48755
Alex Panaev <panaev.as@gmail.com>
2023-02-18 21:01:13 +0400

    PageObject:updated LoginPage

--- src/main/java/com/aqa/LoginPage.java
+++ src/main/java/com/aqa/LoginPage.java
@@ -1,4 +1,11 @@
 package com.aqa;
 
+import com.codeborne.selenide.Selenide;
+
 public class LoginPage {
+
+    public LoginPage openLoginPage(){
+        Selenide.open("/login");
+        return new LoginPage();
+    }
 }
