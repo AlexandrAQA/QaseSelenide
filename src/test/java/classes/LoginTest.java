42518eb27e88d95e3867338f2510acfe578bbac4
Alex Panaev <panaev.as@gmail.com>
2023-02-20 00:10:25 +0400

    Screenshots:changed LoginTest(filter) & pom

--- src/test/java/classes/LoginTest.java
+++ src/test/java/classes/LoginTest.java
@@ -1,17 +1,23 @@
 package classes;
 
 import com.aqa.LoginPage;
+import com.codeborne.selenide.testng.ScreenShooter;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.openqa.selenium.By.id;
 
+@Listeners({ScreenShooter.class})
 public class LoginTest {
 
     @Test
     public void testLogin() {
+        ScreenShooter.captureSuccessfulTests = true;
         new LoginPage().openLoginPage()
                        .login();
 
@@ -19,5 +25,7 @@ public class LoginTest {
         $(id("createButton")).shouldBe(visible);
         //only assert checks
         assertThat($(id("createButton")).getText()).isNotNull().isEqualTo("Create new project");
+        $x("//table").$$x("//tr[@class='project-row']//a")
+                     .filter(exactText("Test"));
     }
 }
