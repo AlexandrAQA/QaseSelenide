f8f5a1cda7cfcdec4fc117742d34128c6f52f052
Alex Panaev <panaev.as@gmail.com>
2023-02-19 20:37:35 +0400

    PageObject:Refactoring LoginTest & LoginPage

--- src/main/java/com/aqa/LoginPage.java
+++ src/main/java/com/aqa/LoginPage.java
@@ -2,10 +2,22 @@ package com.aqa;
 
 import com.codeborne.selenide.Selenide;
 
+import static com.codeborne.selenide.Condition.enabled;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.openqa.selenium.By.id;
+
 public class LoginPage {
 
     public LoginPage openLoginPage(){
         Selenide.open("/login");
+        getWebDriver().manage().window().maximize();
         return new LoginPage();
+    }
+    public LoginPage login(){
+        $(id("inputEmail")).shouldBe(enabled).sendKeys("mrgmgrv@gmail.com");
+        $(id("inputPassword")).sendKeys("Selenide23+");
+        $(id("btnLogin")).click();
+        return new LoginPage(); //TODO Change on ProjectPage
     }
 }
--- src/test/java/classes/LoginTest.java
+++ src/test/java/classes/LoginTest.java
@@ -1,41 +1,23 @@
 package classes;
 
+import com.aqa.LoginPage;
 import org.testng.annotations.Test;
-import static com.codeborne.selenide.Condition.enabled;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.openqa.selenium.By.id;
 
 public class LoginTest {
 
-//    @BeforeClass
-//    public static void setUp(){
-//        Configuration.baseUrl = "https://app.qase.io";
-//    }
-
     @Test
     public void testLogin() {
-        //1.open URL
-        open("/login");
-        //2.Find the elements by ID. FullXpath: //input[@id='inputEmail'/'inputPassword']
-        $(id("inputEmail"))
-                //3.shouldBe(Condition.enabled). => we can verify that elem is enabled
-                //and waiter is here too
-                .shouldBe(enabled)
-                //send email to the field
-                .sendKeys("mrgmgrv@gmail.com");
-
-        $(id("inputPassword")).sendKeys("Selenide23+");
-        $(id("btnLogin")).click();
+        new LoginPage().openLoginPage()
+                       .login();
 
         //'Should' is not checking! it's just for sure
         $(id("createButton")).shouldBe(visible);
-
         //only assert checks
         assertThat($(id("createButton")).getText()).isNotNull().isEqualTo("Create new project");
-
-       // final boolean test = $((WebElement) text("Test")).isDisplayed();//a[text()="Test"]
     }
 }
