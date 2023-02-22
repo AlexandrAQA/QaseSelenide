faff5ece2fad3fc9ba23d2842974b0977cd8a677
Alex Panaev <panaev.as@gmail.com>
2023-02-22 16:47:41 +0400

    REST_API: added fields into Pet

--- src/main/java/com/aqa/model/Pet.java
+++ src/main/java/com/aqa/model/Pet.java
@@ -1,4 +1,12 @@
 package com.aqa.model;
 
+import java.util.List;
+
 public class Pet {
+    public long id;
+    public Category category;
+    public String name;
+    public List<String> photoUrls;
+    public List<Tag> tags;
+    public String status;
 }
