4a5e3490747f3e440f2134a23ce385fdb185ef56
Alex Panaev <panaev.as@gmail.com>
2023-02-22 16:24:24 +0400

    REST_API: Refactoring

--- src/test/java/classes/rest/PetTest.java
+++ src/test/java/classes/rest/PetTest.java
@@ -4,15 +4,14 @@ import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.Matchers.*;
 
 public class PetTest {
 
     @Test
     public void createPetTest(){
-        int id = given().
+        //POST Pet
+        long id = given().
                 contentType(ContentType.JSON).
                 accept(ContentType.JSON).
                 body("{\n" +
@@ -38,9 +37,22 @@ public class PetTest {
                post("https://petstore.swagger.io/v2/pet")
       .then().
               statusCode(200). //check status code
-               body("id",not(empty()),"name", equalTo("MyTestDog2")).
-               log().all(). //to see logs
-               extract().body().jsonPath().getInt("id"); //extract id from the response
+                body("id",not(empty()),"name", equalTo("MyTestDog2")).
+                log().all(). //to see logs
+                extract().body().jsonPath().getLong("id"); //extract id from the response
+
+    //GET PET /pet/{petId}
+        given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                log().all().
+                pathParam("petId",id).
+        when().
+                get("https://petstore.swagger.io/v2/pet/{petId}")
+        .then().
+                statusCode(200).
+                log().body().
+                log().status(); //check status code
 
     }
 }
