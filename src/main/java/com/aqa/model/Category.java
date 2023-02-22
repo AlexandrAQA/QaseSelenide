c222a14d524ef8c706ff87492cda24af85da9d74
Alex Panaev <panaev.as@gmail.com>
2023-02-22 17:00:51 +0400

    added @Data,@AllArgsConstructor(),@Builder from lombok

--- src/main/java/com/aqa/model/Category.java
+++ src/main/java/com/aqa/model/Category.java
@@ -1,5 +1,14 @@
 package com.aqa.model;
 
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class Category {
     public int id;
     public String name;
--- src/main/java/com/aqa/model/Tag.java
+++ src/main/java/com/aqa/model/Tag.java
@@ -1,5 +1,13 @@
 package com.aqa.model;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class Tag {
     public int id;
     public String name;
