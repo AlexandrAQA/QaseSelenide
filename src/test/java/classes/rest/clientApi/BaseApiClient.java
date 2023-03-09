d05d378000b11f4e03b7645c767cc2ab00931084
Alex Panaev <panaev.as@gmail.com>
2023-03-09 14:40:39 +0400

    added RequestSpecification class from restAssured + constructor

--- src/test/java/classes/rest/clientApi/BaseApiClient.java
+++ src/test/java/classes/rest/clientApi/BaseApiClient.java
@@ -2,12 +2,24 @@ package classes.rest.clientApi;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
 public class BaseApiClient {
+
+    //restAssured lib
+    RequestSpecification requestSpecification;
+
+    public BaseApiClient(){
+        requestSpecification = given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                log().ifValidationFails();
+    }
+
 
     public Response post(String uri, Object body){
         return given().
